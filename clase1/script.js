b4eae5766e6e5b00286a39b914db1a98ee6a10bd
nhandhya <ada_hack176@hackademy.mx>
2022-03-04 21:08:07 -0600

    ejercicios 1,2,3 de js

---
+++ clase1/script.js
@@ -0,0 +1,106 @@
+/*const nombre ="Juana de Arco";
+let edad = 15;
+
+
+let condicion = edad >=18;
+
+let condicion2 =nombre === "Juana garza"
+
+
+console.log("condicion =>", condicion);
+
+console.log("condicion2 =>", condicion2 );
+
+
+
+if(condicion){
+  console.log(`entra la primera condicion`);
+} else if (condicion2) {
+    console.log(`entra la segunda condicion`);
+} else {
+console.log("ninguna de las conciciones es verdadera");
+}
+
+//un = es de asignación
+//2 iguales para comparar valor, 3iguales para comparar valor y tipo de dato
+//otra forma es de hacer if else 
+//esto son TERNARIOS  edad >18 ? console.log("es mayor de edad ") : console.log("es menor de edad");
+*/
+//CICLOS
+//paso 1 declarar einicializar una variable 
+//pasar una condicion (hasta que punto nuestro ciclo se va a ejecutar
+//que tanto incrementara la variable
+
+
+
+
+
+
+
+//arreglo: lista para poner in sinfin de variables
+//los arreglos empiexan a contar deade el 0 al igual que el for
+/*const arreglo = ["Juan", "Jesus" , "Maria","Ramon"];
+console.log("arreglo =>", arreglo);
+
+console.log(arreglo[3]);
+
+
+/*for(let indice= 0; indice < arreglo.length; indice++ ){ 
+ console.log(`El nombre de la persona es ${arreglo[indice]}`);  
+}
+*/
+
+
+
+//arreglo.forEach(nombre =>console.log(`El nombre de la persona es ${nombre}`));
+
+
+
+/*let contador = 0;
+
+while (contador< 1000){ 
+  console.log(contador);
+
+  contador = contador + 1;
+}*/
+
+
+//funciones pedazos de codigo encapsulado
+
+/*function Saludar(nombre,apellido){
+   console.log(`Hola ${nombre} ${apellido}, cómo estas?`);
+   
+}*/
+
+
+
+
+/*const persona1 = {
+nombre:"Quico",
+apellido:"Hernandez",
+edad: 34,
+
+};
+
+const persona2 = {
+nombre:"Caracol",
+apellido:"caracolito",
+edad:8,
+
+};
+
+const admitirPaso = (persona) => {
+  if(persona.edad >= 18){
+      console.log(`Puedes pasar ${persona.nombre} ${persona.apellido}`);
+} else
+ {console.log(  `No puedes pasar ${persona.nombre} ${persona.apellido}`);
+ }
+};
+
+
+admitirPaso(persona1);
+admitirPaso(persona2);
+*/
+
+//0,1 , 1,2,3,5,8,13,21,34,55,89,144
+
---
+++ ejercicioJS.js
@@ -0,0 +1,84 @@
+/*const ejercicio1 = [56,8,45,12,10,3,77,100,96,36,5,22,20,31,47,2,50,61
+
+];
+
+const nPrimo = numero =>
+{
+ for (let i = 2; i < numero; i++){
+   if(numero % i == 0)
+   return false;
+
+ }
+return true;
+}
+ejercicio1.forEach(numero =>{
+if(nPrimo(numero))
+{
+console.log(`Este numero es primo ${numero}`);
+}
+})
+*/
+
+
+
+
+
+/*const persona1 = {
+    nombre:"Quico",
+    apellido:"Hernandez",
+    edad: 34,
+    
+    };
+    
+    const persona2 = {
+    nombre:"Caracol",
+    apellido:"caracolito",
+    edad:8,
+    
+    };
+    
+    const ejercicio2 = (persona) => {
+      if(persona.edad >= 18){
+          console.log(`Puedes pasar familiar ${persona.nombre} ${persona.apellido}`);
+    } else
+     {console.log(  `No puedes pasar ${persona.nombre} ${persona.apellido}`);
+     }
+    };
+    
+    
+    ejercicio2(persona1);
+    ejercicio2(persona2);
+    */
+ 
+    function generateFibbonacci(limit){
+        const fib = [0,1,];
+
+
+    
+     
+      for( let i = 2; i < limit; i++){
+        fib[i] = fib [i - 1] + fib[i - 2];
+
+      }
+             
+        return fib  ;
+
+      
+
+
+
+     }
+     const fibiSeries = generateFibbonacci(100);
+      const ul = document.getElementById("fib");
+     
+ fibiSeries.forEach(number => {
+     const li = document.createElement("li");
+      li.innerHTML = number;
+       ul.append(li);
+
+ });
+ 
+ 
+ 
+ 
+ 
